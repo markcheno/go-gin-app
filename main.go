2428119e1e50c413747b6a5c2d9fe42a8a63f920
Mark Chenoweth <mark@cheno.net>
2017-02-28 18:10:57 -0600

    refactored

--- main.go
+++ main.go
@@ -3,9 +3,8 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/markcheno/go-gin-app/handlers"
 )
 
 var router *gin.Engine
@@ -17,6 +16,55 @@ func main() {
 	// Set the router as the default one provided by Gin
 	router = gin.Default()
 
+	// Use the setUserStatus middleware for every route to set a flag
+	// indicating whether the request was from an authenticated user or not
+	router.Use(setUserStatus())
+
+	// Handle the index route
+	router.GET("/", handlers.ShowIndexPage)
+
+	// Group user related routes together
+	userRoutes := router.Group("/u")
+	{
+		// Handle the GET requests at /u/login
+		// Show the login page
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.GET("/login", ensureNotLoggedIn(), handlers.ShowLoginPage)
+
+		// Handle POST requests at /u/login
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.POST("/login", ensureNotLoggedIn(), handlers.PerformLogin)
+
+		// Handle GET requests at /u/logout
+		// Ensure that the user is logged in by using the middleware
+		userRoutes.GET("/logout", ensureLoggedIn(), handlers.Logout)
+
+		// Handle the GET requests at /u/register
+		// Show the registration page
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.GET("/register", ensureNotLoggedIn(), handlers.ShowRegistrationPage)
+
+		// Handle POST requests at /u/register
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.POST("/register", ensureNotLoggedIn(), handlers.Register)
+	}
+
+	// Group article related routes together
+	articleRoutes := router.Group("/article")
+	{
+		// Handle GET requests at /article/view/some_article_id
+		articleRoutes.GET("/view/:article_id", handlers.GetArticle)
+
+		// Handle the GET requests at /article/create
+		// Show the article creation page
+		// Ensure that the user is logged in by using the middleware
+		articleRoutes.GET("/create", ensureLoggedIn(), handlers.ShowArticleCreationPage)
+
+		// Handle POST requests at /article/create
+		// Ensure that the user is logged in by using the middleware
+		articleRoutes.POST("/create", ensureLoggedIn(), handlers.CreateArticle)
+	}
+
 	// Process the templates at the start so that they don't have to be loaded
 	// from the disk again. This makes serving HTML pages very fast.
 	router.LoadHTMLGlob("templates/*")
@@ -26,24 +74,4 @@ func main() {
 
 	// Start serving the application
 	router.Run()
-}
-
-// Render one of HTML, JSON or CSV based on the 'Accept' header of the request
-// If the header doesn't specify this, HTML is rendered, provided that
-// the template name is present
-func render(c *gin.Context, data gin.H, templateName string) {
-	loggedInInterface, _ := c.Get("is_logged_in")
-	data["is_logged_in"] = loggedInInterface.(bool)
-
-	switch c.Request.Header.Get("Accept") {
-	case "application/json":
-		// Respond with JSON
-		c.JSON(http.StatusOK, data["payload"])
-	case "application/xml":
-		// Respond with XML
-		c.XML(http.StatusOK, data["payload"])
-	default:
-		// Respond with HTML
-		c.HTML(http.StatusOK, templateName, data)
-	}
 }
---
+++ models/user_test.go
@@ -0,0 +1,87 @@
+// models.user_test.go
+
+package models
+
+import "testing"
+
+// Test the validity of different combinations of username/password
+func TestUserValidity(t *testing.T) {
+	if !IsUserValid("user1", "pass1") {
+		t.Fail()
+	}
+
+	if IsUserValid("user2", "pass1") {
+		t.Fail()
+	}
+
+	if IsUserValid("user1", "") {
+		t.Fail()
+	}
+
+	if IsUserValid("", "pass1") {
+		t.Fail()
+	}
+
+	if IsUserValid("User1", "pass1") {
+		t.Fail()
+	}
+}
+
+// Test if a new user can be registered with valid username/password
+func TestValidUserRegistration(t *testing.T) {
+	saveLists()
+
+	u, err := RegisterNewUser("newuser", "newpass")
+
+	if err != nil || u.Username == "" {
+		t.Fail()
+	}
+
+	restoreLists()
+}
+
+// Test that a new user cannot be registered with invalid username/password
+func TestInvalidUserRegistration(t *testing.T) {
+	saveLists()
+
+	// Try to register a user with a used username
+	u, err := RegisterNewUser("user1", "pass1")
+
+	if err == nil || u != nil {
+		t.Fail()
+	}
+
+	// Try to register with a blank password
+	u, err = RegisterNewUser("newuser", "")
+
+	if err == nil || u != nil {
+		t.Fail()
+	}
+
+	restoreLists()
+}
+
+// Test the function that checks for username availability
+func TestUsernameAvailability(t *testing.T) {
+	saveLists()
+
+	// This username should be available
+	if !IsUsernameAvailable("newuser") {
+		t.Fail()
+	}
+
+	// This username should not be available
+	if IsUsernameAvailable("user1") {
+		t.Fail()
+	}
+
+	// Register a new user
+	RegisterNewUser("newuser", "newpass")
+
+	// This newly registered username should not be available
+	if IsUsernameAvailable("newuser") {
+		t.Fail()
+	}
+
+	restoreLists()
+}
